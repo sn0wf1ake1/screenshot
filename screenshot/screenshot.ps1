b26a671c3cee1e6a75a881360d9a7b6a87186085
sn0wflake <12665561+sn0wflake@users.noreply.github.com>
2017-09-04 05:08:50 +0200

    File rename

---
+++ screenshot/screenshot.ps1
@@ -0,0 +1,17 @@
+﻿#
+# screenshot.ps1
+#
+
+[string]$FileNamePattern = 'screenshot_{0}.png'
+
+while(1) {
+    $fileName = $FileNamePattern -f (Get-Date).ToString('yyyyMMdd_HHmmss_ffff')
+    $path = Join-Path $PWD $fileName
+
+    $b = New-Object System.Drawing.Bitmap([System.Windows.Forms.Screen]::PrimaryScreen.Bounds.Width, [System.Windows.Forms.Screen]::PrimaryScreen.Bounds.Height)
+
+    $g = [System.Drawing.Graphics]::FromImage($b)
+    $g.CopyFromScreen((New-Object System.Drawing.Point(0,0)), (New-Object System.Drawing.Point(0,0)), $b.Size)
+    $g.Dispose()
+    $b.Save($path)
+}
